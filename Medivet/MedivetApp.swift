8f85cc45d0aa37a47aa6fc0377bdc7bb9866acc6
Marcelina Sitko <xmarcelasx@gmail.com>
2022-10-10 20:40:52 +0200

    init project

---
+++ Medivet/MedivetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedivetApp.swift
+//  Medivet
+//
+//  Created by Marcelina Sitko on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MedivetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
