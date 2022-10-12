44fa704e32d415c9aac1a539ed86719b9374ce4a
Marcelina Sitko <xmarcelasx@gmail.com>
2022-10-12 20:15:43 +0200

    create login screen without functionality

--- Medivet/Commons/Constants/Constants.swift
+++ Medivet/Commons/Constants/Constants.swift
@@ -12,10 +12,17 @@ struct K {
         static let continueAs = "Kontynuuj jako"
         static let vet = "Weterynarz"
         static let patient = "Pacjent"
+        static let noAccountYet = "Nie masz jeszcze konta?"
     }
     
     struct Inputs {
         static let email = "Email"
         static let password = "Hasło"
+    }
+    
+    struct Buttons {
+        static let signIn = "Zaloguj się"
+        static let forgotPassword = "Zapomniałeś hasła?"
+        static let signUp = "Zarejestruj się"
     }
 }
--- Medivet/Views/Screens/LoginScreen.swift
+++ Medivet/Views/Screens/LoginScreen.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct LoginScreen: View {
-    func printDupa() {
-        print("dupa")
+    
+    func login() {
+        print("login")
     }
+   
     var body: some View {
         VStack {
-            MedivetTextInput(icon: "x.circle.fill", placeholder: K.Inputs.email, action:printDupa)
-                MedivetTextInput(icon: "eye.fill", placeholder: K.Inputs.password)
+            MedivetTextInput(icon: "x.circle.fill", placeholder: K.Inputs.email)
+            MedivetTextInput(icon: "eye.fill", placeholder: K.Inputs.password)
+            Button{
+                
+            } label: {
+                Text(K.Buttons.forgotPassword)
+            }.padding(.top, 25)
+                .foregroundColor(Color.gray)
+            MedivetButton(title: K.Buttons.signIn, backgroundColor: Colors.primary, color: Color.white, action: login)
+                .padding(.top, 35)
+            HStack{
+                Text(K.Words.noAccountYet)
+                    .foregroundColor(Colors.secondary)
+                    .font(.system(size: 17))
+                Button{
+                    
+                } label: {
+                    Text(K.Buttons.signUp)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Colors.secondary)
+                }.font(.system(size: 17))
+            }.padding(.top, 25)
         }.padding()
     }
 }
