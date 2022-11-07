0145990085db9c72e498ef0d2151395266cc4ea7
Marcelina Sitko <xmarcelasx@gmail.com>
2022-11-07 22:51:43 +0100

    create number & text form inputs

---
+++ Medivet/Views/Commons/Form/FormNumberInput.swift
@@ -0,0 +1,50 @@
+//
+//  FormNumberInput.swift
+//  Medivet
+//
+//  Created by Marcelina Sitko on 06/11/2022.
+//
+
+import SwiftUI
+
+struct FormNumberInput: View {
+    let label: String
+    @Binding var value: Int?
+    let isClearable: Bool?
+    
+    @ViewBuilder func renderClearIcon() -> some View {
+        if isClearable! {
+            Button {
+                clearValue()
+                print("clear")
+            } label: { Image(systemName: "x.circle.fill").foregroundColor(Color.gray)
+                    .padding(.leading, 10)
+            }.buttonStyle(.borderless)
+        }
+    }
+    
+    func clearValue() {
+        value = nil
+    }
+    
+    var body: some View {
+        HStack {
+            HStack{
+                Text(label)
+                    .frame(minWidth: 120, maxWidth: 120, alignment: .leading)
+                Spacer()
+                TextField(
+                    Translations.Common.optional,
+                    text: Binding<String>(get: {
+                        if value == nil {
+                            return ""
+                        }
+                        return "\(value!)"
+                    }, set: { String in
+                        value = Int(String)
+                    })).keyboardType(.numberPad)
+            }
+            renderClearIcon()
+        }
+    }
+}
---
+++ Medivet/Views/Commons/Form/FormTextInput.swift
@@ -0,0 +1,44 @@
+//
+//  FormInput.swift
+//  Medivet
+//
+//  Created by Marcelina Sitko on 06/11/2022.
+//
+
+import SwiftUI
+
+struct FormTextInput: View {
+    let label: String
+    @Binding var value: String
+    let isClearable: Bool?
+    
+    @ViewBuilder func renderClearIcon() -> some View {
+        if isClearable! {
+            Button {
+                clearValue()
+            } label: { Image(systemName: "x.circle.fill").foregroundColor(Color.gray)
+                    .padding(.leading, 10)
+            }
+        }
+    }
+    
+    func clearValue() {
+        value = ""
+    }
+    
+    var body: some View {
+        HStack {
+            HStack {
+                Text(label)
+                    .frame(minWidth: 120, maxWidth: 120, alignment: .leading)
+                Spacer()
+                TextField(
+                    value == "" ? Translations.Common.optional : "",
+                    text: $value
+                )
+            }
+            Spacer()
+            renderClearIcon()
+        }
+    }
+}
